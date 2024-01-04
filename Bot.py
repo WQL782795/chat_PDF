9b9e6a822b6cdb1b8e90011b70ff8ed9c0c424c9
Wang,Qilin <qilin.wang@zstack.io>
2024-01-04 16:35:45 +0800

    gradio

--- Bot.py
+++ Bot.py
@@ -5,23 +5,25 @@ from db import *
 from text_pre_handle import *
 
 
-def query(query):
+def query(query_text, history):
     rag_data = ''
     context = []
-    result = db_search(query)
-    for re in result:
-        for i, r in enumerate(re):
-            rag_data+= f"Top{i + 1}\nDistance:{r.distance}\nText:{decompress_text(r.entity.text_compress)}\n\n"
-    response = send_llm(build_messages(query, rag_data, context))
-    context.append({'role': 'user', 'content': query})
+    result_data = db_search(query_text)
+    for data in result_data:
+        for i, r in enumerate(data):
+            rag_data += f"Top{i + 1}\nDistance:{r.distance}\nText:{decompress_text(r.entity.text_compress)}\n\n"
+    response = send_llm(build_messages(query_text, rag_data, context))
+    context.append({'role': 'user', 'content': query_text})
     context.append({'role': 'assistant', 'content': response})
     return response
 
 
-demo = gr.Interface(
-    fn=query,
-    inputs=["text", "slider"],
-    outputs=["text"],
-)
-
-demo.launch()
+gr.ChatInterface(query,
+                 chatbot=gr.Chatbot(height=300),
+                 textbox=gr.Textbox(placeholder="welcome to ask me question!", container=False, scale=7),
+                 title="雪中悍刀行",
+                 description="Ask me any question about the book 《雪中悍刀行》",
+                 theme="soft",
+                 retry_btn=None,
+                 undo_btn="Delete Previous",
+                 clear_btn="Clear", ).launch(share=True)
